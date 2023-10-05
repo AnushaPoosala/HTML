34448ca4f56a842b83fc8c3398c3f87135843a38
AnushaPoosala <117409595+AnushaPoosala@users.noreply.github.com>
2023-10-05 08:43:05 -0400

    JQuerySyntax and Selectors
    JQuerySyntax and Selectors

---
+++ Student/JQuerySelectorsOct4..js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+    $("p").css("color","purple");
+    $("button").click(function()
+    {
+        $("#ptagid").html("<b>JQuery has 3 selectors</br>1.$(JQuery alias)</br>2.element(tags)</br>3.action())</b>");
+
+    });
+});
+
